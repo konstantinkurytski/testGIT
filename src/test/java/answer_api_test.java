43f7550d0f9e8198a2dcf44f4bd0369d1ff8ef97
KonstantinKuritskiy <dangeel666@gmail.com>
2024-02-23 15:36:05 +0300

    rest api готовый для pogo

--- src/test/java/answer_api_test.java
+++ src/test/java/answer_api_test.java
@@ -1,46 +1,45 @@
-import common.list;
+import common.listPojo;
 import io.qameta.allure.*;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.matcher.ResponseAwareMatcher;
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-import org.hamcrest.Matcher;
+import org.assertj.core.api.AbstractListAssert;
+import org.assertj.core.api.ObjectAssert;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
 
 import java.util.List;
 
-import static com.google.common.base.Predicates.equalTo;
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.requestSpecification;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class answer_api_test {
 
-        @Test
-        @Description("проверка содержимого ответа в тесте")
-        @Epic(value = "тесты api")
-        @Feature(value = "проверки содержимого")
-        @Story(value = "проверка int")
-        public void test_str1() {
-            List<String> list = given()
-                    .filter(new AllureRestAssured())
-                    .contentType(ContentType.JSON)
-                    .log().all()
-                    .when().get("https://reqres.in/api/users")
-                    .prettyPeek()
-                    .then()
-                    .statusCode(200)
-                    .extract().jsonPath().getList("data.id");
-            Assert.assertEquals(list.get(0), 1,"sdcfvs");
-
-
-    }
     @Test
     @Description("проверка содержимого ответа в тесте")
     @Epic(value = "тесты api")
     @Feature(value = "проверки содержимого")
     @Story(value = "проверка int")
+    public void test_str1() {
+        Integer list = given()
+                .filter(new AllureRestAssured())
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when().get("https://reqres.in/api/users")
+                .prettyPeek()
+                .then()
+                .statusCode(200)
+                .extract().jsonPath().getInt("data[0].id");
+        Assert.assertEquals(list, 1, "sdcfvs");
+
+
+    }
+
+    @Test
+    @Description("проверка содержимого ответа в тесте")
+    @Epic(value = "тесты api")
+    @Feature(value = "проверки содержимого")
+    @Story(value = "проверка list")
     public void test_str() {
         List<String> list2 = given()
                 .filter(new AllureRestAssured())
@@ -51,8 +50,44 @@ public class answer_api_test {
                 .then()
                 .statusCode(200)
                 .extract().jsonPath().getList("data.id");
-        Assert.assertEquals(list2.get(1), 2,"sdcfvs");
-        Assert.assertEquals(list2.get(2), 3,"sdcfvs");
+        Assert.assertEquals(list2.get(1), 2, "sdcfvs");
+        Assert.assertEquals(list2.get(2), 3, "sdcfvs");
 
+    }
+
+    @Test
+    @Description("проверка содержимого ответа в тесте")
+    @Epic(value = "тесты api")
+    @Feature(value = "проверки содержимого")
+    @Story(value = "проверка extract")
+    public void test_str3() {
+        Integer list2 = Math.toIntExact(given()
+                .filter(new AllureRestAssured())
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when().get("https://reqres.in/api/users")
+                .prettyPeek()
+                .then()
+                .statusCode(200)
+                .extract().time());
+        System.out.println(list2);
+        Assert.assertNotEquals(list2,2,"sdc");
+
+    }
+    @Test
+    @Description("проверка содержимого ответа в тесте")
+    @Epic(value = "тесты api")
+    @Feature(value = "проверки содержимого")
+    @Story(value = "проверка extract")
+    public void test_str4() {
+        List<listPojo> list3 = given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when().get("https://reqres.in/api/users")
+                .prettyPeek()
+                .then()
+
+                .extract().jsonPath().getList("data", listPojo.class);
+        Assert.assertEquals(list3.get(1).id,2);
     }
 }
